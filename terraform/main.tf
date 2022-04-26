3cac3648841ce268d5b18dc60b1eec65c01dd692
Aboogie <albrown.ctr@kr.af.mil>
2022-04-26 12:16:50 +0300

    iam module fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,6 @@
 module "iam" { 
     source          = "terraform-aws-modules/iam/aws"
-    version         = "4.22.1"
-
-    create_user     = true 
-
-    name            = "alfred-2"   
+    version         = "4.22.1"  
 }
 
 
